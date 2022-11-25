1ad841c490baa71aaab6bdf7faefd492052a6687
anna-dm <anna-anna-frolova@bk.ru>
2022-11-25 19:41:05 +0300

    first

---
+++ js/shopping-list.js
@@ -0,0 +1,13 @@
+$('li').click(function () {
+  $(this).toggleClass('done')
+})
+
+$('span').click(function (event) {
+  event.stopPropagation();
+  $(this).parent().fadeOut(function () {
+    $(this).remove()
+
+  });
+})
+
+$('input').key
