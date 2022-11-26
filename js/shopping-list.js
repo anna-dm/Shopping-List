c66fd06bb7d044944192380bbd9afa6c9f0feae9
anna-dm <anna-anna-frolova@bk.ru>
2022-11-26 21:36:10 +0300

    shopping list

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,8 +1,8 @@
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
   $(this).toggleClass('done')
 })
 
-$('span').click(function (event) {
+$('ul').on('click', 'span', function (event) {
   event.stopPropagation();
   $(this).parent().fadeOut(function () {
     $(this).remove()
@@ -10,4 +10,10 @@ $('span').click(function (event) {
   });
 })
 
-$('input').key
+$('input').keypress(function (event) {
+  if (event.which === 13) {
+    var itemText = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span>x</span> ' + itemText + '</li>');
+  }
+})
